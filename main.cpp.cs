9c9a3449f6fd39022239adb2030e2155505376f0
AlbertoRodrigues34DS <161747956+AlbertoRodrigues34DS@users.noreply.github.com>
2024-05-06 20:48:06 -0300

    teste
    é noix

---
+++ main.cpp.cs
@@ -0,0 +1,14 @@
+using System;
+namespace atividadedupla
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const string nome = "sávio";
+            const string nome2 ="Alberto";
+
+            Console.WriteLine("primeiro nome: "+ nome +" Segundo nome: "+nome2);
+        }
+    }
+}
