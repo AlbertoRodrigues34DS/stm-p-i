3e915b6741fdfdbdef94908793033b3e4aea7223
AlbertoRodrigues34DS <albertofernandesrodrigues8@gmail.com>
2024-05-10 17:14:37 -0300

    atualizado

--- Porjeto Integrado - Copia/Core/Src/main.c
+++ Porjeto Integrado - Copia/Core/Src/main.c
@@ -29,35 +29,62 @@
 /* USER CODE BEGIN PTD */
 
 // Estrutura para representar uma coordenada no plano
-typedef struct {
-    int x;
-    int y;
-} Point;
-
-typedef enum {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
-} Direction;
 
 
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define SNAKE_MAX_LENGTH 100
+#define BOTAO9 (HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_9) == 0)
+#define BOTAO10 (HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_10) == 0)
+#define BOTAO11 (HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_11) == 0)
+#define BOTAO12 (HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_12) == 0)
+
+#define LED3_LIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_3,1)
+#define LED4_LIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,1)
+#define LED5_LIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,1)
+#define LED6_LIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6,1)
+
+#define LED3_DESLIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_3,0)
+#define LED4_DESLIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,0)
+#define LED5_DESLIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,0)
+#define LED6_DESLIGA HAL_GPIO_WritePin(GPIOB,GPIO_PIN_6,0)
+
+#define DELAY HAL_Delay(2000)
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
+int direcao;
+int ColetavelX;
+int ColetavelY;
+int score = 0;
+int scoremax;
+int controle = 0;
+int velo = 100;
+int botao;
+int bola1Inix = 83;
+int bola1Iniy = 150;
+int bola1Finx = 83;
+int bola1Finy = 150;
 
+int bola2x = 74;
+int bola2y = 150;
+
+int bola3x = 65;
+int bola3y = 150;
+
+int bola4x = 56;
+int bola4y = 150;
+
+int jogar = 0;
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 SPI_HandleTypeDef hspi1;
 
 /* USER CODE BEGIN PV */
+
 
 /* USER CODE END PV */
 
@@ -66,91 +93,23 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI1_Init(void);
 /* USER CODE BEGIN PFP */
-
+void Menu(void);
+void IniciarJogo (void);
+void VelocidadeLed (void);
+void Controles(void);
+void movimento(void);
+void desenharbola1(void);
+void desenharbola2(void);
+void desenharbola3(void);
+void desenharbola4(void);
+void apagarbola(void);
+void colisao(void);
+void coletavel(void);
+void apagarcoletavel(void);
 // Atualiza a posição da cabeça da cobrinha com base na direção atual
 
-struct Snake {
-    Point body[SNAKE_MAX_LENGTH];
-    int length;
-    Direction direction;
-};
 
 
-
-
-    void game_init(struct Snake *snake);
-	void update_snake_position(struct Snake *snake);
-	void draw_snake(struct Snake *snake);
-	int check_collision(struct Snake *snake);
-
-	void game_init(struct Snake *snake);
-
-	void game_init(struct Snake *snake) {
-	    // Configurações iniciais do hardware
-	    ST7789_Init();
-
-	    // Inicialização da cobrinha
-	    snake->length = 1;
-	    snake->body[0].x = 120;
-	    snake->body[0].y = 120;
-	    snake->direction = RIGHT;
-	}
-
-
-	void update_snake_position(struct Snake *snake) {
-	// Atualiza a posição da cabeça da cobrinha com base na direção atual
-
-    switch (snake->direction) {
-        case UP:
-            snake->body[0].y--;
-            break;
-        case DOWN:
-            snake->body[0].y++;
-            break;
-        case LEFT:
-            snake->body[0].x--;
-            break;
-        case RIGHT:
-            snake->body[0].x++;
-            break;
-    }
-    // Atualiza a posição do corpo da cobrinha movendo cada segmento
-    for (int i = snake->length - 1; i > 0; i--) {
-            snake->body[i] = snake->body[i - 1];
-	}
-
-    void draw_snake(struct Snake* snake) {
-        // Limpa a tela
-        ST7789_FillScreen(BLACK);
-
-        // Desenha cada segmento da cobrinha
-        for (int i = 0; i < snake->length; i++) {
-            ST7789_DrawPixel(snake->body[i].x, snake->body[i].y, WHITE);
-        }
-
-        // Atualiza a tela
-        ST7789_Refresh();
-    }
-
-    int check_collision(struct Snake *snake) {
-
-        // Verifica colisão com as bordas da tela
-
-        if (snake->body[0].x < 0 || snake->body[0].x >= 240 || snake->body[0].y < 0 || snake->body[0].y >= 240) {
-            return 1; // Colisão com as bordas
-        }
-
-        // Verifica colisão com o próprio corpo da cobrinha
-
-        for (int i = 1; i < snake->length; i++) {
-            if (snake->body[0].x == snake->body[i].x && snake->body[0].y == snake->body[i].y) {
-                return 1; // Colisão com o próprio corpo
-            }
-        }
-
-        return 0; // Sem colisão
-    }
-}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -189,28 +148,23 @@ int main(void)
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
   ST7789_Init();
-  struct Snake snake;
-  	    game_init(&snake);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  //Menu();
+	  Jogo();
 
-	           update_snake_position(&snake);
-	  	        draw_snake(&snake);
-	  	        if (check_collision(&snake)) {
-	  	            // Game over
-	  	            break;
 	  	        }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
 
+  /* USER CODE END 3 */
+
+}
 /**
   * @brief System Clock Configuration
   * @retval None
@@ -233,8 +187,7 @@ void SystemClock_Config(void)
   }
   /** Initializes the CPU, AHB and APB buses clocks
   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -327,8 +280,154 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
-
+int posx = 40;
+int posy = 120;
+int raio = 5;
 /* USER CODE BEGIN 4 */
+void Jogo(){
+	 posx = 40;
+	 posy = 120;
+	 raio = 5;
+
+	int contador = 1;
+
+	ST7789_Fill_Color(BLACK);
+	ST7789_DrawFilledRectangle(20,40,200,180, WHITE);
+
+	while(contador){
+		if(BOTAO11 && posx < 190){
+			MVdireita();
+		}
+		if(BOTAO9 && posx > 20){
+			MVesquerda();
+			}
+
+		if(BOTAO10){
+
+			contador = 0;
+		}
+		DesenhaHorizonte();
+	}
+}
+void DesenhaHorizonte(){
+	raio =0
+	while(true){
+
+		ST7789_DrawFilledCircle(posx + raio, posy, 5, BLACK);
+
+	}
+
+}
+void DesenhaVertical(){
+
+	ST7789_DrawFilledCircle(posx , (posy + raio), 5, BLACK);
+	ST7789_DrawFilledCircle(posx , (posy + (raio * 3)), 5, BLACK);
+	ST7789_DrawFilledCircle(posx , (posy+ (raio * 4)), 5, BLACK);
+	ST7789_DrawFilledCircle(posx, (posy+ (raio * 2)), 5, BLACK);
+	ST7789_DrawFilledCircle(posx, (posy + (raio * 2)), 5, BLACK);
+	ST7789_DrawFilledCircle(posx, (posy+ (raio * 2)), 5, BLACK);
+}
+
+void MVdireita(){
+	while(posx < 190){
+					ST7789_DrawFilledCircle(posx, posy, 5, WHITE);
+					DesenhaHorizonte();
+			posx = posx + 5;
+			if(BOTAO10){
+				ST7789_DrawFilledCircle(posx - (raio * 2), posy, 5, WHITE);
+				ST7789_DrawFilledCircle(posx - (raio * 3), posy, 5, WHITE);
+				ST7789_DrawFilledCircle((posx - (raio * 4)), (posy), 5, WHITE);
+				ST7789_DrawFilledCircle(posx - (raio * 5), posy, 5, WHITE);
+				ST7789_DrawFilledCircle((posx - (raio * 6)), (posy), 5, WHITE);
+				MVbaixo();
+				break;
+			}	if(BOTAO12){
+				ST7789_DrawFilledCircle(posx - (raio * 2), posy, 5, WHITE);
+				ST7789_DrawFilledCircle(posx - (raio * 3), posy, 5, WHITE);
+				ST7789_DrawFilledCircle((posx - (raio * 4)), (posy), 5, WHITE);
+				ST7789_DrawFilledCircle(posx - (raio * 5), posy, 5, WHITE);
+				ST7789_DrawFilledCircle((posx - (raio * 6)), (posy), 5, WHITE);
+				MVcima();
+				break;
+			}
+				}
+
+}
+void MVesquerda(){
+
+	while(posx > 20){
+	ST7789_DrawFilledCircle((posx + (raio * 8)), (posy), 5, WHITE);
+	DesenhaHorizonte();
+	posx = posx - 5;
+	if(BOTAO10){
+		ST7789_DrawFilledCircle(posx + (raio * 2), posy, 5, WHITE);
+		ST7789_DrawFilledCircle(posx + (raio * 3), posy, 5, WHITE);
+		ST7789_DrawFilledCircle((posx + (raio * 4)), (posy), 5, WHITE);
+		ST7789_DrawFilledCircle(posx + (raio * 5), posy, 5, WHITE);
+		ST7789_DrawFilledCircle((posx + (raio * 6)), (posy), 5, WHITE);
+		MVbaixo();
+		break;
+	}	if(BOTAO12){
+		ST7789_DrawFilledCircle(posx + (raio * 2), posy, 5, WHITE);
+		ST7789_DrawFilledCircle(posx + (raio * 3), posy, 5, WHITE);
+		ST7789_DrawFilledCircle((posx + (raio * 4)), (posy), 5, WHITE);
+		ST7789_DrawFilledCircle(posx + (raio * 5), posy, 5, WHITE);
+		ST7789_DrawFilledCircle((posx + (raio * 6)), (posy), 5, WHITE);
+		MVcima();
+		break;
+	}
+	}
+}
+void MVcima(){
+
+	while(posy > 40){
+	ST7789_DrawFilledCircle(posx, (posy + (raio * 8)), 5, WHITE);
+	DesenhaVertical();
+	posy = posy - 5;
+	if(BOTAO11){
+		ST7789_DrawFilledCircle(posx , (posy + (raio * 3)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx , (posy+ (raio * 4)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy+ (raio * 2)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy + (raio * 2)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy+ (raio * 2)), 5, WHITE);
+		MVdireita();
+		break;
+	}	if(BOTAO9){
+		ST7789_DrawFilledCircle(posx , (posy + (raio * 3)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx , (posy+ (raio * 4)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy+ (raio * 2)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy + (raio * 2)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy+ (raio * 2)), 5, WHITE);
+		MVesquerda();
+		break;
+	}
+	}
+}
+void MVbaixo(){
+
+	while(posy < 220){
+	ST7789_DrawFilledCircle((posx + (raio * 8)), (posy), 5, WHITE);
+	DesenhaVertical();
+	posy = posy + 5;
+	if(BOTAO11){
+		ST7789_DrawFilledCircle(posx , (posy - (raio * 3)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx , (posy - (raio * 4)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy - (raio * 2)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy  - (raio * 2)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy - (raio * 2)), 5, WHITE);
+		MVdireita();
+		break;
+	}	if(BOTAO9){
+		ST7789_DrawFilledCircle(posx , (posy - (raio * 3)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx , (posy - (raio * 4)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy - (raio * 2)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy - (raio * 2)), 5, WHITE);
+		ST7789_DrawFilledCircle(posx, (posy - (raio * 2)), 5, WHITE);
+		MVesquerda();
+		break;
+	}
+	}
+}
 
 /* USER CODE END 4 */
 
@@ -343,6 +442,7 @@ void Error_Handler(void)
   __disable_irq();
   while (1)
   {
+
   }
   /* USER CODE END Error_Handler_Debug */
 }
